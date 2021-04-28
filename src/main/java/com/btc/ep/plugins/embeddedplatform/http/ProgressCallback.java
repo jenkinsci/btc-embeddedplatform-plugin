0365281c4faec1e2c70ac8f06860a2267806f132
Thabo Krick <thabo.krick@btc-es.de>
2021-04-28 18:32:01 +0200

    removed experimental files

--- src/main/java/com/btc/ep/plugins/embeddedplatform/http/ProgressCallback.java
+++
@@ -1,9 +0,0 @@
-package com.btc.ep.plugins.embeddedplatform.http;
-
-public interface ProgressCallback {
-
-    void updateProgressValue(int value);
-
-    void updateProgressMessage(String message);
-
-}
